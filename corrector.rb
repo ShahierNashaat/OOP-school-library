4a2e109619e0f0956501c07065fa9f8317aa05b3
Shahier Nashaat <shahiern@yahoo.com>
2022-01-25 13:00:26 +0200

    Create Corrector class

---
+++ corrector.rb
@@ -0,0 +1,5 @@
+class Corrector
+  def correct_name(name)
+    name.capitalze[0, 10].strip
+  end
+end
--- person.rb
+++ person.rb
@@ -1,3 +1,5 @@
+require 'corrector'
+
 class Person
   attr_reader :id
   attr_accessor :name, :age
@@ -7,10 +9,15 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @corrector = Corrector.new
   end
 
   def can_use_services?
     return true if of_age? || @parent_permission
+  end
+
+  def validate_name
+    @corrector.correct_name(@name)
   end
 
   private
