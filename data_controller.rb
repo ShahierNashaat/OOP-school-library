7f0c8e9517a8f34f3fe222000a5b16cd7e730354
Shahier Nashaat <shahiern@yahoo.com>
2022-02-01 15:08:11 +0200

    Fix reading data error

--- data_controller.rb
+++ data_controller.rb
@@ -10,12 +10,12 @@ module DataController
     data = []
     if File.exist?(file) && File.read(file) != ''
       JSON.parse(File.read(file)).each do |element|
-        if element.data_type == 'Teacher'
-          data.push(Teacher.new(id: element.id, age: element.age, specialization: element.specialization,
-                                name: element.name))
+        if element['data_type'] == 'Teacher'
+          data.push(Teacher.new(id: element['id'].to_i, age: element['age'].to_i,
+                                specialization: element['specialization'], name: element['name']))
         else
-          data.push(Student.new(id: element.id, age: element.age, name: element.name,
-                                parent_permission: element.parent_permission))
+          data.push(Student.new(id: element['id'].to_i, age: element['age'].to_i, name: element['name'],
+                                parent_permission: element['parent_permission']))
         end
       end
     end
@@ -27,18 +27,18 @@ module DataController
     data = []
     if File.exist?(file) && File.read(file) != ''
       JSON.parse(File.read(file)).each do |element|
-        data.push(Book.new(element.title, element.author))
+        data.push(Book.new(element['title'], element['author']))
       end
     end
     data
   end
 
   def get_person_by_id(id)
-    @persons.map { |element| element.id == id }
+    @persons.each { |element| return element if element.id == id.to_i }
   end
 
   def get_book_by_title(title)
-    @books.map { |_elemet| element.title == title }
+    @books.each { |element| return element if element.title == title }
   end
 
   def load_rentals
@@ -46,9 +46,10 @@ module DataController
     data = []
     if File.exist?(file) && File.read(file) != ''
       JSON.parse(File.read(file)).each do |element|
-        person = get_person_by_id(element.person_id)
-        book = get_book_by_title(element.book_title)
-        data.push(Rental.new(element.date, book, person))
+        person = get_person_by_id(element['person_id'])
+        book = get_book_by_title(element['book_title'])
+        puts book
+        data.push(Rental.new(element['date'], book, person))
       end
     end
     data
@@ -59,28 +60,28 @@ module DataController
     @persons.each do |person|
       if person.instance_of?(Teacher)
         data.push({ id: person.id, age: person.age, name: person.name,
-          specialization: person.specialization, data_type: person.class })
+                    specialization: person.specialization, data_type: person.class })
       else
         data.push({ id: person.id, age: person.age, name: person.name,
-          parent_permission: person.parent_permission, data_type: person.class })
+                    parent_permission: person.parent_permission, data_type: person.class })
       end
     end
-        File.write("people.json", JSON.generate(data))
+    File.write('people.json', JSON.generate(data))
   end
 
   def save_books
     data = []
     @books.each do |book|
-      data.push({title: book.title, author: book.author})
+      data.push({ title: book.title, author: book.author })
     end
-    File.write("books.json", JSON.generate(data))
+    File.write('books.json', JSON.generate(data))
   end
 
   def save_rental
     data = []
     @rentals.each do |rental|
-      data.push({date: rental.date, book_title: rental.book.title, person_id: rental.person.id})
+      data.push({ date: rental.date, book_title: rental.book.title, person_id: rental.person.id })
     end
-    File.write("rentals.json", JSON.generate(data))
+    File.write('rentals.json', JSON.generate(data))
   end
 end
